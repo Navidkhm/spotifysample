6eb686995368a37722199aa777cb30f3f69e1460
M.Navid <51302086+Navidkhm@users.noreply.github.com>
2021-01-14 21:04:17 +0330

    Update DetailsModel.js

--- models/DetailsModel.js
+++ models/DetailsModel.js
@@ -4,7 +4,7 @@ class DetailsModel{
 
      album = [['Tenet','Ludwig Göransson','The plan','Foils','Sator'],['Soul',
      'Cody, ChesnuttDaveed, MacDougallWilliam, Hutson','Lost Soul','Terry Time','Rappin Ced','Falling','Thank You'],
-     ['Barmigardam','Arman Garshasby','Barmigardam'],['thoughts','Shahrdad Rouhani','thoughts']]
+     ['Barmigardam','Arman Garshasby','Barmigardam']]
      
     @observable pic = '/images/barmigardam.jpg';
     @observable name = 'Barmigardam';
